b8a887a1014b2a1fa255e8deb1f7eea139de5d75
Карлинский Александр <mrhardcorovec@yandex.ru>
2021-03-09 15:26:28 +0300

    Исправил названия переменных

--- Sortirovka/Program.cs
+++ Sortirovka/Program.cs
@@ -4,21 +4,24 @@ namespace Sortirovka
 {
     class Program
     {
-        static int[] sortirovka(int[] mas)
+        static int[] Sorting(int[] mas)
         {
-            int zamena;
+            int replacement;
             for (int j = 0; j < mas.Length; j++)
             {
                 for (int l = j + 1; l < mas.Length; l++)
                 {
                     if (mas[j] > mas[l])
                     {
-                        zamena = mas[j];
+                        replacement = mas[j];
                         mas[j] = mas[l];
-                        mas[l] = zamena;
+                        mas[l] = replacement;
                     }
                 }
             }
+            Console.WriteLine(" ");
+            Console.Write("Отсортированный массив");
+            Console.WriteLine(" ");
             for (int o = 0; o < mas.Length; o++)
             {
                 Console.Write(mas[o] + " ");
@@ -31,45 +34,62 @@ namespace Sortirovka
         {
             Console.WriteLine("Привествую! Выберите метод ввода элементов сортировки:" + "\r\n" + "1. Вводить самому элементы массива" + "\r\n" + "2.Включить рандомайзер для элементов" + "\r\n" +
                                 "Примечание: в случае выбора рандомайзера, элементы массива будут валироваться от 0 до 100");
-            int nastroika_vvoda = Convert.ToInt32(Console.ReadLine());
-            if (nastroika_vvoda == 1)
+            int input_setting;
+            while (!int.TryParse(Console.ReadLine(), out input_setting))
             {
-                Console.WriteLine("Введите кол-во элементов массива, которые вы будете вводить:");
-                int kolichestvo = Convert.ToInt32(Console.ReadLine());
-                int[] massive = new int[kolichestvo];
-                Console.WriteLine("Вводите элементы для массива:");
-                for (int i = 0; i < kolichestvo; i++)
-                {
-                    massive[i] = Convert.ToInt32(Console.ReadLine());
-                }
-                sortirovka(massive);
-                /*for (int o = 0; o < kolichestvo; o++)
-                {
-                    Console.Write(massive[o] + " ");
-                }*/
+                Console.WriteLine("Ошибка ввода! Введите 1 или 2");
             }
-            else if (nastroika_vvoda == 2)
+            if (input_setting == 1)
             {
-                Console.WriteLine("Введите кол-во элементов массива:");
-                int kolichestvo = Convert.ToInt32(Console.ReadLine());
-                int[] massive = new int[kolichestvo];
-                Console.WriteLine("Ваш массив");
-                for (int i = 0; i < kolichestvo; i++)
-                {
-                    Random rnd = new Random();
-                    int alement = rnd.Next(0, 100);
-                    massive[i] = alement;
-                    Console.Write(massive[i] + " ");
-                }
-                Console.WriteLine(" ");
-                Console.Write("Отсортированный массив");
-                Console.WriteLine(" ");
-                sortirovka(massive);
-                /*for (int o = 0; o < kolichestvo; o++)
-                {
-                    Console.Write(massive[o] + " ");
-                }*/
+                Choise1();
             }
+            else if (input_setting == 2)
+            {
+                int[] massive = Choise2();
+                Sorting(massive);
+            }
+        }
+
+        private static int[] Choise2()
+        {
+            Console.WriteLine("Введите кол-во элементов массива:");
+            int quantity;
+            while (!int.TryParse(Console.ReadLine(), out quantity))
+            {
+                Console.WriteLine("Ошибка ввода! Введите цифровое значение");
+            }
+            int[] massive = new int[quantity];
+            Console.WriteLine("Ваш массив");
+            for (int i = 0; i < quantity; i++)
+            {
+                Random rnd = new Random();
+                int alement = rnd.Next(0, 100);
+                massive[i] = alement;
+                Console.Write(massive[i] + " ");
+            }
+
+            return massive;
+        }
+
+        private static void Choise1()
+        {
+            Console.WriteLine("Введите кол-во элементов массива, которые вы будете вводить:");
+            int quantity;
+            while (!int.TryParse(Console.ReadLine(), out quantity))
+            {
+                Console.WriteLine("Ошибка ввода! Введите цифровое значение");
+            }
+            int[] massive = new int[quantity];
+            Console.WriteLine("Вводите элементы для массива:");
+            for (int i = 0; i < quantity; i++)
+            {
+                massive[i] = Convert.ToInt32(Console.ReadLine());
+                while (!int.TryParse(Console.ReadLine(), out massive[i]))
+                {
+                    Console.WriteLine("Ошибка ввода! Введите цифровое значение");
+                }
+            }
+            Sorting(massive);
         }
     }
 }
